59362a34178915522d01113e0f14384c85f19dbc
Subin Yook <sprout.jessie@gmail.com>
2022-08-18 23:57:37 +0900

    solving

--- level-01/008-최대공약수와최소공배수.js
+++ level-01/008-최대공약수와최소공배수.js
@@ -12,13 +12,20 @@ function solution(n, m) {
   var answer = [];
 
   let a = 0;
+  let b = 0;
 
-  for (let i = 0; i < n; i++) {
-    a++;
-    if (n % i == 0) {
-      return a;
+  let c = n < m ? m : n;
+
+  for (let i = 1; i <= c; i++) {
+    if (n % i == 0 && m % i == 0) {
+      a = i;
     }
   }
+
+  b = (n * m) / a;
+
+  answer = [a, b];
+  return answer;
 }
 
-//
+// https://celltong.tistory.com/entry/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-JavaScript-level-1-%EC%B5%9C%EB%8C%80%EA%B3%B5%EC%95%BD%EC%88%98%EC%99%80-%EC%B5%9C%EC%86%8C%EA%B3%B5%EB%B0%B0%EC%88%98
