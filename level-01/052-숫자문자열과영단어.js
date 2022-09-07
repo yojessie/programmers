43cfa54a9adbeb8ca5e3e279853b22b86439f9a8
Subin Yook <sprout.jessie@gmail.com>
2022-09-07 23:02:20 +0900

    solved

--- level-01/052-숫자문자열과영단어.js
+++ level-01/052-숫자문자열과영단어.js
@@ -1,2 +1,28 @@
 // 2022-09-07
 // https://school.programmers.co.kr/learn/courses/30/lessons/81301
+
+function solution(s) {
+  let txt = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  while (isNaN(s)) {
+    for (let i = 0; i < txt.length; i++) {
+      s = s.replace(txt[i], i);
+    }
+  }
+
+  return Number(s);
+}
+
+// 별것도 아닌것을 오늘의 엉망진창인 컨디션에 파뭍혀서 해결하지 못하고있었다.
+// isNaN로 불리언 값을 불러올 수 있다는 것도 배웠다.
