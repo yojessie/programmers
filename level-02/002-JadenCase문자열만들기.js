47d4f409883ccd4f72bd6bb1fc997f88cac66ee8
yojessie <sprout.jessie@gmail.com>
2022-09-09 23:58:41 +0900

    solving

--- level-02/002-JadenCase문자열만들기.js
+++ level-02/002-JadenCase문자열만들기.js
@@ -1,2 +1,11 @@
 // 2022-09-08
 // https://school.programmers.co.kr/learn/courses/30/lessons/12951
+
+function solution(s) {
+  return s
+    .toLowerCase()
+    .split(" ")
+    .forEach((x) => x[0].toUpperCase());
+}
+
+// 왜 toUpperCase가 적용이 안되지..?
