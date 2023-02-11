167aaf63173b66c63e86f4d1584ae10282f41490
yojessie <sprout.jessie@gmail.com>
2023-02-12 07:45:11 +0900

    solved

--- level-00/043-치킨쿠폰.js
+++ level-00/043-치킨쿠폰.js
@@ -4,14 +4,25 @@
 function solution(chicken) {
   let answer = 0;
   let left = 0;
-  while (chicken > 0) {
-    left += chicken % 10;
+  while (chicken > 9) {
+    left = chicken % 10;
     chicken = Math.floor(chicken / 10);
     answer += chicken;
+    chicken += left;
   }
-  while (left > 0) {
-    left = Math.floor(left / 10);
-    answer += left;
+
+  return answer;
+}
+
+// 아래 풀이가 더 명확한 것 같다.
+
+function solution(chicken) {
+  let answer = 0;
+  let coupon = chicken;
+
+  while (coupon > 9) {
+    answer += Math.floor(coupon / 10);
+    coupon = Math.floor(coupon / 10) + (coupon % 10);
   }
 
   return answer;
